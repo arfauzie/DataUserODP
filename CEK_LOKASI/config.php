b5f6685b95ab46154d04c6b5b02e441949f354bc
arfauzie <alirfauzie77@gmail.com>
2025-09-22 08:05:19 +0700

    p

--- CEK_LOKASI/config.php
+++ CEK_LOKASI/config.php
@@ -1,9 +1,9 @@
 <?php
-// config.php
+
 $host = "localhost";
-$user = "root";       // sesuaikan username MySQL kamu
-$pass = "";           // sesuaikan password MySQL kamu
-$db   = "msn_db";     // database utama
+$user = "root";
+$pass = "";
+$db   = "msn_db";
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
